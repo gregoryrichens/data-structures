99681188640a881e054234d618f93dc45c78e87b
Gregory Richens <gregoryrichens@gmail.com>
2021-01-27 02:09:10 -0700

    initial function setup and notes

---
+++ Binary Search & Graphs/shortestRouteMeshMessage.js
@@ -0,0 +1,8 @@
+function shortestRoute(network, sender, recipient) {
+  // write a dynamic programming function
+  // initiate a sender variable
+  // initiate a recipient variable
+  // initiate an array to store path
+
+  // recursive helper function for finding route
+}
