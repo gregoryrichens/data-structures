b1078e4390a7a39f7b8a1904933be1033abc2298
Gregory Richens <gregoryrichens@gmail.com>
2021-01-31 20:03:25 -0700

    reviewed Mesh Message - bfs practice

--- graphs/shortestRouteMeshMessage.js
+++ graphs/shortestRouteMeshMessage.js
@@ -56,3 +56,44 @@ function bfsGetPath(graph, startNode, endNode) {
   // so there's NO path from startNode to endNode
   return null;
 }
+
+function reversePath(bfsPath, start, end) {
+  let currentNode = end;
+  let reversedPath = [];
+
+  while (currentNode !== null) {
+    reversedPath.push(currentNode);
+    currentNode = bfsPath[currentNode];
+  }
+
+  return reversedPath.reverse();
+}
+
+function shortestRoute (network, startNode, endNode) {
+  if(!network.hasOwnProperty(startNode)) { throw new Error('startNode not in graph') }
+  if(!network.hasOwnProperty(endNode)) { throw new Error('endNode not in graph') }
+
+  let path = {};
+  path[startNode] = null;
+
+  let queue = [];
+  queue.push(startNode);
+
+  while (queue.length > 0) {
+    // shift the first item from the queue
+    let currentNode = queue.shift();
+
+    // if we reach the end then we need to reverse the path
+    if (currentNode === endNode) { return reversePath(path, startNode, endNode) }
+    // otherwise for all neighbors of the node that haven't been visited
+    network[currentNode].forEach(neighbor => {
+      // add it to visited
+      // add to queue
+      if(!path.hasOwnProperty(neighbor)) {
+        path[neighbor] = currentNode;
+        queue.push(neighbor);
+      }
+    })
+  }
+
+}
