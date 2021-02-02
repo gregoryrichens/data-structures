0e8c3bd137e562082443ab5b96242615beb5619e
Gregory Richens <gregoryrichens@gmail.com>
2021-02-02 16:32:37 -0700

    recursive get change

--- dynamic programming/makeChange.js
+++ dynamic programming/makeChange.js
@@ -16,3 +16,37 @@ let getChange = function(targetAmount, denominations) {
 }
 
 console.log(getChange(235, [1,5,10,25,100]));
+
+function getChangeRecursive(targetAmount, denominations, currentIndex) {
+  let storage = {};
+  // define a recursive function
+  function recursiveHelper(amountLeft, denominations, currentIndex) {
+    // if amountLeft is saved in storage, return that answer
+    let key = [amountLeft, currentIndex].join(', ');
+    if (storage.hasOwnProperty(key)) {
+      return storage[key];
+    }
+    // exit case we found the amount
+    if (amountLeft === 0) { return 1 };
+    // exit casae we overshot
+    if (amountLeft < 0) { return 0 };
+    // exit case out of coins
+    if (index > denominations.length) { return 0 };
+
+    // current coin
+    let currentCoin = denominations[currentIndex];
+    // number of possibilities
+    let numPossibilities = 0;
+
+    while (amountLeft >= 0) {
+      numPossibilities += recursiveHelper(amountLeft, denominations, currentIndex + 1);
+      amountLeft -= currentCoin;
+    }
+
+    // save in memo
+    storage[key] = numPossibilities;
+    // return answer
+    return numPossibilities;
+  }
+  // return the recursive function
+}
