067fc38605ecce875d35bf515d38bb787a6d1831
Gregory Richens <gregoryrichens@gmail.com>
2021-02-05 16:22:30 -0700

    used set and arguments to find duplicates in arguments in linear time

---
+++ randomProblems/frequency counter/duplicateArguments.js
@@ -0,0 +1,18 @@
+function areThereDuplicates() {
+  // good luck. (supply any arguments you deem necessary.)
+  // use a set for storage
+  let storage = new Set();
+  for(let i=0; i < arguments.length; i++) {
+      if(!storage.has(arguments[i])) {
+          storage.add(arguments[i]);
+      } else {
+          storage.delete(arguments[i]);
+      }
+  }
+  if(storage.size !== arguments.length) { return true }
+  // if it's not in the set add it
+  // if it is in the set, take it out
+  // if the length of arguments and the size of the set are different return true
+  // otherwise return false
+  return false;
+}
