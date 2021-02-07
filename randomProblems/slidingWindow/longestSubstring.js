097a275ea5973f62158963e35e9e9c63f59feeb3
Gregory Richens <gregoryrichens@gmail.com>
2021-02-07 03:16:08 -0700

    more sensible formulation of longest unique substring

--- randomProblems/slidingWindow/longestSubstring.js
+++ randomProblems/slidingWindow/longestSubstring.js
@@ -4,14 +4,15 @@ function findLongestSubstring(str) {
   let start = 0;
 
   for (let i = 0; i < str.length; i++) {
+    if (i === 5) { debugger }
     let char = str[i];
-    if (seen[char]) {
-      start = Math.max(start, seen[char]);
+    if (seen.hasOwnProperty(char)) {
+      start = Math.max(start, (seen[char] + 1));
     }
     // index - beginning of substring + 1 (to include current in count)
     longest = Math.max(longest, i - start + 1);
     // store the index of the next char so as to not double count
-    seen[char] = i + 1;
+    seen[char] = i;
   }
   return longest;
 }
