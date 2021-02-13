04ebdf0267143b10bd3bac7ebc6b3674a28278aa
Gregory Richens <gregoryrichens@gmail.com>
2021-02-13 01:31:19 -0700

    review all permutations of a string recursive

--- dynamic programming/stringPermutations.js
+++ dynamic programming/stringPermutations.js
@@ -26,3 +26,29 @@ console.log(findAllPermutations('blake'));
 // let testString = 'mamma';
 // let testSet = new Set([testString]);
 // console.log(testSet);
+
+// the way we solve this one recursively is to recurse on
+// everything but the last character continually
+// take off the last character
+// find all permutations of everything but the last character
+// for each permuration
+// we go through as many times as the permutaiton is long
+// substituting the last character from above at every step and adding to the set of permutations
+function findAllPermutationsPractice(string) {
+  if(string.length <= 1) { return new Set(string)}
+
+  let allCharactersExceptLast = string.slice(0,-1);
+  let lastCharacter = string[string.length - 1];
+  let permutations = new Set();
+
+  let allPermutationsExceptLast = findAllPermutationsPractice(allCharactersExceptLast);
+
+  allPermutationsExceptLast.forEach((aPermutationExceptForLast) => {
+    for (let i = 0; i < aPermutationExceptForLast.length; i++){
+      let permutation = aPermutationExceptForLast.slice(0,i) + lastCharacter + aPermutationExceptForLast.slice(i);
+      permutations.add(permutation);
+    }
+  });
+
+  return permutations;
+}
