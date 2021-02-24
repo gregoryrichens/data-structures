4a45216f64b00cd0ff70fcdb2f70c326687a6644
Gregory Richens <gregoryrichens@gmail.com>
2021-02-24 04:07:08 -0700

    is valid binary search tree iterative practice

--- Binary Search & Graphs/validBinarySearch.js
+++ Binary Search & Graphs/validBinarySearch.js
@@ -42,3 +42,41 @@ function recursiveValidBS(treeNode, minValue = Number.NEGATIVE_INFINITY, maxValu
 
   return recursiveValidBS(treeNode.left, minValue, treeNode.value) && recursiveValidBS(treeNode.right, treeNode.value, maxValue);
 }
+
+isBinarySearchTreeIterative(root) {
+  // create stack
+  let nodeStack = [];
+  nodeStack.push({
+    node: root,
+    lowerBound: Number.NEGATIVE_INFINITY,
+    upperBound: Number.POSITIVE_INFINITY,
+  })
+  // while there is a stack
+  while(nodeStack.length) {
+    let nodeInQuestion = nodeStack.pop();
+  // if value is less then lower bound or greater than upper bound return false
+    if(nodeInQuestion.node.value <= nodeInQuestion.lowerBound || nodeInQuestion.node.value >= nodeInQuestion.upperBound) {
+      return false;
+    }
+  // if there is a left
+    if (nodeInQuestion.node.left) {
+    // push onto the stack with value, lower bound is lower bound, upper bound is noode value
+      nodeStack.push({
+        node: nodeInQuestion.node.left,
+        upperBound: nodeInQuestion.node.value,
+        lowerBound: nodeInQuestion.node.lowerBound,
+      });
+    }
+  // if there is a right
+    if (nodeInQuestion.node.right) {
+    // push onto the stack with upper bound, and lower bound as node value
+      nodeStack.push({
+        node: nodeInQuestion.node.right,
+        upperBound: nodeInQuestion.node.upperBound,
+        lowerBound: nodeInQuestion.node.value,
+      });
+    }
+  }
+  // return true
+  return true;
+}
